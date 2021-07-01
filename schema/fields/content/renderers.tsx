29a343eeae66c68cd42ca5f69fdab494897f450c
mitchellhamilton <mitchell@hamil.town>
2021-07-01 13:01:45 +1000

    Voting

--- schema/fields/content/renderers.tsx
+++ schema/fields/content/renderers.tsx
@@ -3,10 +3,14 @@ import {
   DocumentRenderer as KeystoneDocumentRenderer,
   DocumentRendererProps,
 } from '@keystone-next/document-renderer';
-import { ComponentProps } from 'react';
+import React, { ComponentProps, Fragment } from 'react';
 
 import { H1, H2, H3, H4, H5, H6, P } from '../../../components/ui/typography';
 import { Divider } from '../../../components/ui/layout';
+import { useAuth } from '../../../components/auth';
+import { Button } from '../../../components/ui/controls';
+import { Link } from '../../../components/ui/link';
+import { gql, useMutation, useQuery } from 'urql';
 
 // by default the DocumentRenderer will render unstyled html elements
 // we're customising how headings are rendered here but you can customise any of the renderers that the DocumentRenderer uses
@@ -45,11 +49,8 @@ type Answer = {
 type Poll = {
   id: string;
   label: string;
-  data: {
-    label: string;
-    answers: Answer[];
-    userAnswer: { id: string };
-  };
+  answers: Answer[];
+  userAnswer: { id: string } | null;
 };
 
 const calloutStyles = {
@@ -85,24 +86,89 @@ export const componentBlockRenderers: InferRenderersForComponentBlocks<
   },
   poll: function Poll({ poll: relatedPoll }) {
     if (!relatedPoll?.data) return null;
-    const poll = relatedPoll as Poll;
-    // return <pre>{JSON.stringify(poll)}</pre>;
+    const [{ data }] = useQuery({
+      query: gql`
+        query ($id: ID!) {
+          Poll(where: { id: $id }) {
+            id
+            label
+            answers {
+              id
+              label
+              voteCount
+            }
+            userAnswer {
+              id
+            }
+          }
+        }
+      `,
+      variables: { id: relatedPoll.id },
+    });
+    const poll = (data?.Poll || relatedPoll.data) as Poll;
+
+    const [{}, voteForPoll] = useMutation(gql`
+      mutation ($answerId: ID!) {
+        voteForPoll(answerId: $answerId)
+      }
+    `);
+    const [{}, clearVoteForPoll] = useMutation(gql`
+      mutation ($pollId: ID!) {
+        clearVoteForPoll(pollId: $pollId)
+      }
+    `);
+    const auth = useAuth();
     return (
       <div className="my-4">
         <div className="text-grey-800 uppercase text-lg font-semibold">
-          {poll.data.label}
+          {poll.label}
         </div>
-        {poll.data.answers.map(answer => {
-          return (
-            <div
-              key={answer.id}
-              className="text-grey-800 mt-2 flex items-center"
-            >
-              <span className="rounded-full bg-blue-200 border-2 border-blue-400 w-4 h-4 inline-block mr-4" />
-              <span className="cursor-pointer">{answer.label}</span>
-            </div>
-          );
-        })}
+        <form>
+          {poll.answers.map(answer => {
+            return (
+              <label
+                key={answer.id}
+                className="text-grey-800 mt-2 flex items-center"
+              >
+                <input
+                  type="radio"
+                  name={poll.id}
+                  value={answer.id}
+                  checked={poll.userAnswer?.id === answer.id}
+                  disabled={!auth.ready || !auth.sessionData}
+                  onChange={() => {
+                    voteForPoll(
+                      { answerId: answer.id },
+                      { additionalTypenames: ['Poll', 'PollAnswer'] }
+                    );
+                  }}
+                  className="rounded-full bg-blue-200 border-2 border-blue-400 w-4 h-4 inline-block mr-4"
+                />
+                <span className="cursor-pointer">
+                  {answer.label} {answer.voteCount} Votes
+                </span>
+              </label>
+            );
+          })}
+        </form>
+        {auth.ready && !auth.sessionData && (
+          <Fragment>
+            <Link href="/signin">Sign In</Link> or{' '}
+            <Link href="/signup">Join</Link> to vote
+          </Fragment>
+        )}
+        {poll.userAnswer?.id && (
+          <Button
+            onClick={() => {
+              clearVoteForPoll(
+                { pollId: relatedPoll.id },
+                { additionalTypenames: ['Poll', 'PollAnswer'] }
+              );
+            }}
+          >
+            Clear Vote
+          </Button>
+        )}
       </div>
     );
   },
