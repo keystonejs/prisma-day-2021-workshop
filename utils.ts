03349beeca42edfe34dd27aadf31a13fe7eca2ad
Jonathan Weinreich <jonathan@fourcube.net>
2021-10-13 05:52:33 +0100

    Monadic logging. Prototype api key warnings. Logging and base algebra. new home is utils

--- utils.ts
+++ utils.ts
@@ -1,9 +1,39 @@
 import { keystoneNextjsBuildApiKey } from './keystone';
 
+// Fix Me: Test code: Needs another home.
+//This is one place where any means it! The intent was object dumping code, down to
+//every leaf. A testground for DRY logging in ts.
+// Closures are good for logging, localising to particular files/functions/lines ... the issues of this short segment quickly shows why.
+// I have some multicol markup for logging, it bash though, so replaces console.log.
+// A markup standard for shell would be useful.
+
+const log = (s: string) => console.log(s);
+//Very bad code, has to go soon. Monadic replacement.
+const rmap_va =
+  (...props: any) =>
+  (f: any) => {
+    props.forEach((element: any) => {
+      f(element);
+    });
+  };
+
+let colors = require('colors/safe');
+export const success = (...obj: any) =>
+  rmap_va(obj)((x: any) => log(colors.green(x.toString())));
+
+export const warn = (...obj: any) =>
+  rmap_va(obj)((x: any) => log(colors.yellow(x.toString())));
+
+export const report_security_incident = (...obj: any) =>
+  rmap_va(obj)((x: any) => log(colors.red(x.toString())));
+
+export const report_error = (...obj: any) =>
+  rmap_va(obj)((x: any) => log(colors.red(x.toString())));
+
 //Useful for monadic typing. WIP
 const unit = {};
 
-//Logical combinators
+//Logical combinators: applicative, so type save.
 const ftrue = (a: any) => (b: any) => (c: any) => a(c);
 const ffalse = (a: any) => (b: any) => b;
 const fkeep = (a: any) => a;
@@ -17,89 +47,63 @@ const keepIf = (val: any) => (!!val ? fkeep : ffalse);
 // FIXME: Strongly type me please! Basically its a string or a monad applied to a string, with the terminating string used to terminate props
 // and additional input to the render function to abstract away the names.
 // FIXME: Parameterise the type so that it becomes a general purpose monadic compiler.
-// Note: Applicative anys are typesafe.
-
+// Note: Applicative anys are typesafe
+// Very quickly, thanks to Church et al, all core ts operators have been abstracted into a functional algebra, forcing the use of primitives that are known to work in adverse situations.
+// This lower the burnen of proof on security audits, since the code now has monadic/monoid structure, which is uniform to code in.
 const pure_string_monad =
   (termPredicate: any) =>
   (termString: any) =>
   (transferFun: any) =>
   (renderFun: any) =>
   (state: string) =>
-  (next_delta: string): any =>
-    next_delta === termString
-      ? renderFun(termString)(state)
-      : pure_string_monad(termPredicate)(termString)(transferFun)(renderFun)(
-          transferFun(state)(next_delta)
-        );
+  (next_delta: string) =>
+    termPredicate(next_delta)(termString)(renderFun(termString)(state))(
+      pure_string_monad(termPredicate)(termString)(transferFun)(renderFun)(
+        transferFun(state)(next_delta)
+      )
+    );
 
-const cmpString = (termString: string) => (next_delta: string) =>
-  next_delta === termString;
+const fcmpString = (termString: string) => (next_delta: string) =>
+  funIf(next_delta === termString);
+
 const spaceJoinStrings = (a: string) => (b: string) => a + ' ' + b;
 
 const renderTailwind = (cname: string) => (tailwindCompositeStyle: string) =>
   cname + '=' + tailwindCompositeStyle;
+
 const renderState = (cname: string) => (state: string) => state;
-const renderLog = (cname: string) => (tailwindCompositeStyle: string) =>
-  console.log(tailwindCompositeStyle);
 
-const tailwindMonad =
-  pure_string_monad(cmpString)('class')(spaceJoinStrings)(renderTailwind);
+export const renderLog = (cname: string) => (logLine: string) =>
+  console.log(logLine);
 
-//Specialise for each file
-const emitLog = 'emitLog';
-const logMonadClos = (fileName: string) =>
-  pure_string_monad(cmpString)(emitLog)(spaceJoinStrings)(renderLog);
+export const tailwindMonad =
+  pure_string_monad(fcmpString)('class')(spaceJoinStrings)(renderTailwind);
 
-const utilsLog = logMonadClos('utils');
+//Specialise for a file
+export const logMonadClos = (fileName: string) =>
+  pure_string_monad(fcmpString)(emitLog)(spaceJoinStrings)(renderLog);
 
-// The above is a fully featured monadic tailwind compiler :)
-
-//Very bad code, has to go soon. Monadic replacement.
-const rmap_va =
-  (...props: any) =>
-  (f: any) => {
-    props.forEach((element: any) => {
-      f(element);
-    });
-  };
-
-let colors = require('colors/safe');
-
-// Fix Me: Test code: Needs another home.
-//This is one place where any means it! The intent was object dumping code, down to
-//every leaf. A testground for DRY logging in ts.
-// Closures are good for logging, localising to particular files/functions/lines ... the issues of this short segment quickly shows why.
-// I have some multicol markup for logging, it bash though, so replaces console.log.
-// A markup standard for shell would be useful.
-
-const log = (s: string) => console.log(s);
-
-export const success = (...obj: any) =>
-  rmap_va(obj)((x: any) => log(colors.green(x.toString())));
-
-export const warn = (...obj: any) =>
-  rmap_va(obj)((x: any) => log(colors.yellow(x.toString())));
-
-export const report_security_incident = (...obj: any) =>
-  rmap_va(obj)((x: any) => log(colors.red(x.toString())));
-
-export const report_error = (...obj: any) =>
-  rmap_va(obj)((x: any) => log(colors.red(x.toString())));
-
-export const gql = ([content]: TemplateStringsArray) => content;
+export const successCol = colors.green;
+export const warningCol = colors.yellow;
+export const errorCol = colors.red;
 
 //Testing the new logging api.
-export const localWarning = (a: string) => utilsLog(colors.yellow(a));
-export const localError = (a: string) => utilsLog(colors.red(a));
-export const localSuccess = (a: string) => utilsLog(colors.green(a));
+const emitLog = 'emitLog';
+const utilsLog = logMonadClos('utils');
+const utilsWarning = (a: string) => utilsLog(warningCol(a));
+const utilsError = (a: string) => utilsLog(errorCol(a));
+const utilsSuccess = (a: string) => utilsLog(successCol(a));
+// The above is a fully featured monadic tailwind compiler :)
+
+export const gql = ([content]: TemplateStringsArray) => content;
 
 export async function fetchGraphQL_inject_api_key(
   query: string,
   variables?: Record<string, any>
 ) {
   funIf(keystoneNextjsBuildApiKey.includes('keystone'))(
-    localWarning('utils: prototype api key: ' + keystoneNextjsBuildApiKey)
-  )(localSuccess('Next build triggered: Api key injected.'))(emitLog);
+    utilsWarning('utils: prototype api key: ' + keystoneNextjsBuildApiKey)
+  )(utilsSuccess('Next build triggered: Api key injected.'))(emitLog);
 
   return fetch('http://localhost:3000/api/graphql', {
     method: 'POST',
