6ccc584340eef2e34af4e39173eaaee59c900b82
Jonathan Weinreich <jonathan@fourcube.net>
2021-10-15 02:37:55 +0100

    Confirmed no throws in event handling. Annotated wrap_any

--- wrap_any.tsx
+++ wrap_any.tsx
@@ -1,27 +1,38 @@
-// Welcome to wrap_any: The aim is for this file to become empty!
-// It's a place to admit temporary defeat in eliminating an `any` type,
-// and clean the api a level higher by providing a concrete type.
+// Welcome to wrap_any: The aim is for this file to become empty
+// It's the place to admit temporary defeat in eliminating an `any` type,
+// and clean the api at a level higher by providing concrete types
 
 
 
-// This is really a JSX type, not sure how to define it yet.
+// To ensure we only match any once in the top level of this app,
+// and give any a descriptive type name
+// This is the local alias for the dreaded `type`
 
-// To ensure we only match any once in this module
-// And give any a properly descriptive type name
 type WrappedRampantlyPolymorphic = any
-
-export type TestingHardenedAny = WrappedRampantlyPolymorphic
-
-export type HardenedAny = WrappedRampantlyPolymorphic
-
+//Any any is immediately assigned the status RampantlyPolymorphic, which matches exactly what it means
 export type RampantlyPolymorphic = WrappedRampantlyPolymorphic
 
+//Anything that really needs to be fully polymorphic has to earn the right
+//Its first status is TestingHardenedAny
+export type TestingHardenedAny = WrappedRampantlyPolymorphic
+
+//Then, once the code has been tested and fully working with undefined input, it can be promoted to
+export type HardenedAny = WrappedRampantlyPolymorphic
+
+
+
+// These are the problem functions
+// The task is to strongly type them. Some require upstream modifications
+// Others are just puzzling, and might have a local solution.
+
+// A JSX type?
 export type DocumentType = WrappedRampantlyPolymorphic
 
+// These have taken hours of time to resolve, with no joy, and have found a temporary home here ... 
 export type ItemType = WrappedRampantlyPolymorphic
-
 export type ItemSession = WrappedRampantlyPolymorphic
 
-export type GraphQLInput = Record<string, any>;
+//From deep down in the keystone types ...
+export type GraphQLInput = Record<string, WrappedRampantlyPolymorphic>;
 
 
