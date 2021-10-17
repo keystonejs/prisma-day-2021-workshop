bd789772d6aa93c3b46b192353a79e0b796c51d2
Jonathan Weinreich <jonathan@fourcube.net>
2021-10-17 23:33:12 +0100

    Added xlog to comment out logging monads.

--- keystone.ts
+++ keystone.ts
@@ -2,7 +2,6 @@ import { log, xlog } from './utils/logging';
 import { config } from '@keystone-next/keystone';
 import { statelessSessions } from '@keystone-next/keystone/session';
 import { createAuth } from '@keystone-next/auth';
-
 import { lists, extendGraphqlSchema } from './schema';
 import { permissions } from './schema/access';
 
