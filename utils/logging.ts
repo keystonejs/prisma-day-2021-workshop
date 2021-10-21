2f01eae1910f8c5a7e7aa5fa1c67f46dd3833fdf
Jonathan Weinreich <jonathan@fourcube.net>
2021-10-22 00:55:54 +0100

    Logging abbrev. fix

--- utils/logging.ts
+++ utils/logging.ts
@@ -151,7 +151,7 @@ export class logclos {
     return logContextInfoGen(this)(simpleLogger)(fix)(stackRenderer)(a);
   }
   info(a: HardenedAny): this {
-    return logContextInfoGen(this)(simpleLogger)(fix)(fileLineRenderer)(a);
+    return logContextInfoGen(this)(simpleLogger)(fix)(this.renderer())(a);
   }
   reportSecurityIncident(a: HardenedAny): this {
     return logContextInfo(this)(errorCol)(this.renderer())(a);
