da81c6e41643259b2c7c985dbe6ed81424f7263f
Jonathan Weinreich <jonathan@fourcube.net>
2021-10-31 04:45:42 +0000

    removed cms.dockerfile now main file working

--- schema/fields/githubRepos/components.tsx
+++ schema/fields/githubRepos/components.tsx
@@ -19,7 +19,7 @@ export type Repo = {
 const Star = () => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns="https://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
